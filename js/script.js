4b5533e1cabd294ca0ff6bc57ba6002980584032
Bohannon-first <spartacus215@yandex.ru>
2020-11-23 20:58:18 +0300

    add

--- js/script.js
+++ js/script.js
@@ -7,17 +7,24 @@ let numberElements = document.querySelector('.number-elements');
 
 btnCreate.onclick = function () {
     for (let i = 0; i < +numberElements.value; i++) {
+
         elem = document.createElement('p');
         elem.textContent = "Новый абзац...";
         elem.classList.add('elem');
+
+        if (i == 0) {
+            elem.style.backgroundColor = "darkred";
+            elem.style.color = "white";
+        }
 
         newElements.append(elem);
     }
 }
 
+// let children = newElements.childNodes;
+
 btnRemove.onclick = function () {
-    for (let ) {
-
+    for (let i = newElements.childNodes.length - 1; i > -1; i--) {
+        newElements.removeChild(newElements.childNodes[i]);
     }
-
 }
