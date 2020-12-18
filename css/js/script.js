b6f13fe1b9cf6993950edff102933b781e2d3b1b
Ярослав <spartacus215@yandex.ru>
2020-12-18 07:24:25 +0300

    Сделал

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+let login = document.querySelector('.login');
+let modal = document.querySelector('.form-wrap');
+
+login.addEventListener('click', function () {
+    modal.classList.add('show')
+});
