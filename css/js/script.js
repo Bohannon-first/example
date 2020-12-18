6c7b4992b3d3b2339c984d23b77af3ddaba9378c
Ярослав <spartacus215@yandex.ru>
2020-12-18 07:30:10 +0300

    Удалил скрипт

--- css/js/script.js
+++
@@ -1,6 +0,0 @@
-let login = document.querySelector('.login');
-let modal = document.querySelector('.form-wrap');
-
-login.addEventListener('click', function () {
-    modal.classList.add('show')
-});
